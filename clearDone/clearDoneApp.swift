b7be497b06906f8dc05c37f5c7a3f928929cf3c9
JMu <64600882+JettM9104@users.noreply.github.com>
2025-05-18 13:20:37 -0400

    Initial Commit

---
+++ clearDone/clearDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  clearDoneApp.swift
+//  clearDone
+//
+//  Created by Jett Mu on 2025-05-18.
+//
+
+import SwiftUI
+
+@main
+struct clearDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ clearDoneTests/clearDoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  clearDoneTests.swift
+//  clearDoneTests
+//
+//  Created by Jett Mu on 2025-05-18.
+//
+
+import Testing
+@testable import clearDone
+
+struct clearDoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
